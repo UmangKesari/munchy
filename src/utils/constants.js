95999fadbe4dd9cfec2b0e2461007383e01956a5
UmangKesari <umang.kesari17@gmail.com>
2024-04-06 00:07:26 +0530

    Added API_URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const API_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.4529334&lng=81.8348882&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
