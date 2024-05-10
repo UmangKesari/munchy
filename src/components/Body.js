24260c5773348fdc8cb8473108e282f121b7a34a
UmangKesari <umang.kesari17@gmail.com>
2024-05-10 10:29:28 +0530

    Added Higher order component for addinng discount label on restaurant card

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,114 +1,124 @@
-  import RestaurantCard from "./RestaurantCard";
-  import resList from "../utils/mockData";
-  import { useState, useEffect } from "react";
-  import { API_URL } from "../utils/constants";
-  import Shimmer from "./Shimmer";
-  import { Link } from "react-router-dom";
+import RestaurantCard, { withDiscountInfo } from "./RestaurantCard";
+import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
+import { API_URL } from "../utils/constants";
+import Shimmer from "./Shimmer";
+import { Link } from "react-router-dom";
 
-  import useOnlineStatus from "../utils/useOnlineStatus";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
-  const Body = () => {
-    // let [listOfRestaurants, setListOfRestaurants] = useState([
-    //   {
-    //     data: {
-    //       id: "121603",
-    //       resName: "Kannur Food Point",
-    //       cloudinaryImageId: "bmwn4n4bn6n1tcpc8x2h",
-    //       cuisines: ["Kerala", "Chinese"],
-    //       costForTwo: 30000,
-    //       deliveryTime: 24,
-    //       avgRating: "3.8",
-    //     },
-    //   },
-    //   {
-    //     data: {
-    //       id: "428",
-    //       resName: "Biryani Pot",
-    //       cloudinaryImageId: "mdipoyzfzsa7n7igskht",
-    //       cuisines: ["North Indian", "Biryani"],
-    //       costForTwo: 50000,
-    //       deliveryTime: 24,
-    //       avgRating: "4.5",
-    //     },
-    //   },
-    //   {
-    //     data: {
-    //       id: "229",
-    //       resName: "Meghana Foods",
-    //       cloudinaryImageId: "xqwpuhgnsaf18te7zvtv",
-    //       cuisines: [
-    //         "Biryani",
-    //         "Andhra",
-    //         "South Indian",
-    //         "North Indian",
-    //         "Chinese",
-    //         "Seafood",
-    //       ],
-    //       costForTwo: 30000,
-    //       deliveryTime: 24,
-    //       avgRating: "3.8",
-    //     },
-    //   },
-    // ]);
+const Body = () => {
+  // let [listOfRestaurants, setListOfRestaurants] = useState([
+  //   {
+  //     data: {
+  //       id: "121603",
+  //       resName: "Kannur Food Point",
+  //       cloudinaryImageId: "bmwn4n4bn6n1tcpc8x2h",
+  //       cuisines: ["Kerala", "Chinese"],
+  //       costForTwo: 30000,
+  //       deliveryTime: 24,
+  //       avgRating: "3.8",
+  //     },
+  //   },
+  //   {
+  //     data: {
+  //       id: "428",
+  //       resName: "Biryani Pot",
+  //       cloudinaryImageId: "mdipoyzfzsa7n7igskht",
+  //       cuisines: ["North Indian", "Biryani"],
+  //       costForTwo: 50000,
+  //       deliveryTime: 24,
+  //       avgRating: "4.5",
+  //     },
+  //   },
+  //   {
+  //     data: {
+  //       id: "229",
+  //       resName: "Meghana Foods",
+  //       cloudinaryImageId: "xqwpuhgnsaf18te7zvtv",
+  //       cuisines: [
+  //         "Biryani",
+  //         "Andhra",
+  //         "South Indian",
+  //         "North Indian",
+  //         "Chinese",
+  //         "Seafood",
+  //       ],
+  //       costForTwo: 30000,
+  //       deliveryTime: 24,
+  //       avgRating: "3.8",
+  //     },
+  //   },
+  // ]);
 
-    //let listOfRestaurantsJS = [];
-    const [listOfRestaurants, setListOfRestaurants] = useState([]);
-    const [searchText, setSearchText] = useState("");
-    const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  //let listOfRestaurantsJS = [];
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
 
-    useEffect(() => {
-      fetchData(); // callback fun
-    }, []);
+  const RestaurantCardDiscounted = withDiscountInfo(RestaurantCard);
 
-    const fetchData = async () => {
-      const data = await fetch(API_URL);
-      const json = await data.json();
+  useEffect(() => {
+    fetchData(); // callback fun
+  }, []);
 
-      //console.log(json);
-      const restaurants =
-        json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
-          ?.restaurants;
-      setListOfRestaurants(restaurants);
-      setFilteredRestaurant(restaurants);
-    };
+  const fetchData = async () => {
+    const data = await fetch(API_URL);
+    const json = await data.json();
 
-    const onlineStatus = useOnlineStatus();
+    //console.log(json);
+    const restaurants =
+      json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants;
+    setListOfRestaurants(restaurants);
+    setFilteredRestaurant(restaurants);
+  };
 
-    if (onlineStatus === false) {
-      return <h1>You're off line. Please check your internet connection</h1>;
-    }
-    //conditional rendering
-    if (listOfRestaurants.length === 0) {
-      return <Shimmer />;
-    }
-    return (
-      <div className="body">
-        <div className="flex">
-          <div className="search p-4 m-4">
-            <input
-              type="text"
-              className="border border-solid border-black"
-              value={searchText}
-              onChange={(e) => {
-                setSearchText(e.target.value);
-              }}
-            />
-            <button className="px-5 py-1 m-4 bg-green-600 rounded-md"
-              onClick={() => {
-                //
-                console.log(searchText);
-                const filterRes = listOfRestaurants.filter((restaurant) =>
-                  restaurant.info.name
-                    .toLowerCase()
-                    .includes(searchText.toLowerCase())
-                );
-                setFilteredRestaurant(filterRes);
-              }}
-            >
-              Search
-            </button>
-          </div>
-          <div className="p-4 m-4 flex items-center">
+  const onlineStatus = useOnlineStatus();
+
+  function isNonEmptyAggregatedDiscount(obj) {
+    return obj?.info.hasOwnProperty('aggregatedDiscountInfoV3') && 
+      Object.keys(obj?.info?.aggregatedDiscountInfoV3).length > 0
+  }
+
+  if (onlineStatus === false) {
+    return <h1>You're off line. Please check your internet connection</h1>;
+  }
+  //conditional rendering
+  if (listOfRestaurants.length === 0) {
+    return <Shimmer />;
+  }
+
+  console.log(listOfRestaurants);
+  return (
+    <div className="body">
+      <div className="flex">
+        <div className="search p-4 m-4">
+          <input
+            type="text"
+            className="border border-solid border-black"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="px-5 py-1 m-4 bg-green-600 rounded-md"
+            onClick={() => {
+              //
+              console.log(searchText);
+              const filterRes = listOfRestaurants.filter((restaurant) =>
+                restaurant.info.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurant(filterRes);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <div className="p-4 m-4 flex items-center">
           <button
             className="px-5 py-1 m-4 bg-gray-400 rounded-md"
             type="button"
@@ -122,27 +132,35 @@
             }}
             //onMouseOver={() => console.log("move over")}
           >
-              Top Rated Restaurant
+            Top Rated Restaurant
           </button>
-          </div>
-        </div>
-
-        <div className="flex flex-wrap">
-          {
-            // this is how we render dynamic data
-            filteredRestaurant.map((restaurant) => (
-              <Link
-                key={restaurant?.info?.id}
-                to={"/restaurants/" + restaurant.info.id}
-              >
-                <RestaurantCard resData={restaurant} />
-              </Link>
-            ))
-          }
         </div>
       </div>
-    );
-  };
+
+      <div className="flex flex-wrap">
+        {
+          // this is how we render dynamic data
+          filteredRestaurant.map((restaurant) => (
+            <Link
+              key={restaurant?.info?.id}
+              to={"/restaurants/" + restaurant.info.id}
+            >
+              {/* Based on Discount info, corresponding card would be generated */}
+              { 
+                //console.log(restaurant && restaurant.info && restaurant.info.aggregatedDiscountInfoV3)
+                //console.log(Object.keys(restaurant?.info?.aggregatedDiscountInfoV3))
+              }
+              {isNonEmptyAggregatedDiscount(restaurant)? (
+               <RestaurantCardDiscounted resData={restaurant}  />
+              ) : (
+                <RestaurantCard resData={restaurant} />
+              )}
+            </Link>
+          ))
+        }
+      </div>
+    </div>
+  );
+};
 
 export default Body;
-  
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,27 +1,41 @@
 import { CDN_URL } from "../utils/constants";
 
 const RestaurantCard = (props) => {
+  //console.log(props);
+  //const {resData, cuisine} = props; // destructuring
+  const { resData } = props;
+  const { name, cloudinaryImageId, cuisines, avgRating, costForTwo, sla } =
+    resData?.info;
+  return (
+    <div className="p-4 m-4 w-[250px] rounded-lg bg-gray-200 hover:bg-gray-300">
+      <img
+        className="rounded-md"
+        alt="res-image"
+        src={CDN_URL + cloudinaryImageId}
+      />
 
-    //console.log(props);
-    //const {resData, cuisine} = props; // destructuring
-    const { resData } = props;
-    const { name, cloudinaryImageId, cuisines, avgRating, costForTwo, sla } = resData?.info;
+      <h3 className="font-bold py-4 text-lg">{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4> {costForTwo}</h4>
+      <h4>{sla?.deliveryTime} minutes</h4>
+    </div>
+  );
+};
+
+export const withDiscountInfo = (RestaurantCard) => {
+  return (props) => {
+    console.log("prp", props);
+    const { header, subHeader } = props?.resData?.info?.aggregatedDiscountInfoV3;
     return (
-        <div className="p-4 m-4 w-[250px] rounded-lg bg-gray-200 hover:bg-gray-300">
-            <img 
-                className="rounded-md"
-                alt="res-image"
-                src={CDN_URL+cloudinaryImageId}/>
-            
-            <h3 className="font-bold py-4 text-lg">{name}</h3>
-            <h4>{cuisines.join(", ")}</h4>
-            <h4>{avgRating} stars</h4>
-            <h4> {costForTwo}</h4>
-            <h4>{sla?.deliveryTime} minutes</h4>
-
-            
-        </div>
+      <div>
+        <label className="bg-green-600 absolute text-white font m-2 p-2 rounded-lg">
+          { header+subHeader}
+        </label>
+        <RestaurantCard {...props} />
+      </div>
     );
+  };
 };
 
 export default RestaurantCard;
